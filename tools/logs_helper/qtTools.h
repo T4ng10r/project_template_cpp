e0356b7eb15414d52eaabd8134c97807d9fa1e83
Sebastian Mosiej <sebastian.mosiej@wp.pl>
2018-04-17 10:33:21 +0200

    Add logs_helper

---
+++ tools/logs_helper/qtTools.h
@@ -0,0 +1,7 @@
+#ifndef _QT_TOOLS_INCLUDE_
+#define _QT_TOOLS_INCLUDE_
+#include <QtCore/QString>
+
+void logConnection(const QString & strClassName,const QString &strConnDesc,bool bResult);
+
+#endif
